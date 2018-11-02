dd05173d3599fc72840968b54bc727b29f100284
jcworkma <jack.workman@intel.com>
2018-11-01 17:37:43 -0700

    adding batch_scraping.sh for fine-grained control over scraping

---
+++ batch_scraping.sh
@@ -0,0 +1,57 @@
+# df['msd_artist'].str[0].unique()
+# array(['K', 'H', 'Y', 'D', 'J', '3', 'T', 'W', 'L', 'S', 'E', 'R', 'N',
+#       'M', 'C', 'A', 'P', 'F', 'V', 'B', 'O', 'X', 'G', 'I', 'U', 'Z',
+#       '9', '2', 'Å', '*', 'i', '7', 'É', '6', '&', '8', '?', 'Q', '1',
+#       'm', '5', '[', 'z', 'd', 'Á', 'j', '4', 's', 'a', "'", 'p', 't',
+#       'y', 'Ä', 'l', 'k', 'n', 'f', 'c', 'h', 'e', 'x', 'r', '(', 'b',
+#       '.', '-', 'ü', 'Â', 'g', '+', 'Ó', 'v', 'µ', 'Í', '$', '!', 'Ö',
+#       '0', 'u', 'w', 'o', '#', ':', 'Æ', '}', '\x1c'], dtype=object)
+# python scrape_lyrics.py -a a
+# python scrape_lyrics.py -a b
+# python scrape_lyrics.py -a c
+# python scrape_lyrics.py -a d
+# python scrape_lyrics.py -a e
+# python scrape_lyrics.py -a g
+# python scrape_lyrics.py -a h
+# python scrape_lyrics.py -a i
+# python scrape_lyrics.py -a j
+# python scrape_lyrics.py -a k
+# python scrape_lyrics.py -a l
+python scrape_lyrics.py -a m
+python scrape_lyrics.py -a n
+python scrape_lyrics.py -a o
+python scrape_lyrics.py -a p
+python scrape_lyrics.py -a q
+python scrape_lyrics.py -a r
+python scrape_lyrics.py -a s
+# python scrape_lyrics.py -a t
+# python scrape_lyrics.py -a u
+# python scrape_lyrics.py -a v
+# python scrape_lyrics.py -a w
+# python scrape_lyrics.py -a x
+# python scrape_lyrics.py -a y
+# python scrape_lyrics.py -a z
+# python scrape_lyrics.py -a 0
+# python scrape_lyrics.py -a 1
+# python scrape_lyrics.py -a 2
+# python scrape_lyrics.py -a 3
+# python scrape_lyrics.py -a 4
+# python scrape_lyrics.py -a 5
+# python scrape_lyrics.py -a 6
+# python scrape_lyrics.py -a 7
+# python scrape_lyrics.py -a 8
+# python scrape_lyrics.py -a 9
+# python scrape_lyrics.py -a \*
+# python scrape_lyrics.py -a \&
+# python scrape_lyrics.py -a \?
+# python scrape_lyrics.py -a \[
+# python scrape_lyrics.py -a \'
+# python scrape_lyrics.py -a \(
+# python scrape_lyrics.py -a \.
+# python scrape_lyrics.py -a \-
+# python scrape_lyrics.py -a \+
+# python scrape_lyrics.py -a \$
+# python scrape_lyrics.py -a \!
+# python scrape_lyrics.py -a \#
+# python scrape_lyrics.py -a \:
+# python scrape_lyrics.py -a \}
