3a01f854ba8724fb736308c64d5982647e4ec7ea
workmanjack <jworkman16@gmail.com>
2018-11-01 20:28:38 -0700

    now successfully pulling lyrics with genius api

--- scrape_lyrics.py
+++ scrape_lyrics.py
@@ -4,6 +4,7 @@ uses the lyricgenius package to retrieve the lyrics for each song from
 Genius.com. The lyrics are then saved as json files.
 """
 import lyricsgenius as genius
+import time
 import csv
 import os
 # api = genius.Genius()
@@ -13,19 +14,18 @@ EXAMPLE_URL = 'http://lyrics.wikia.com/wiki/Warren_Zevon:Werewolves_of_london'
 
 FILE_MUSIXMATCH_MAPPING = 'data/musixmatch_matches/mxm_779k_matches.txt'
 CSV_MUSIXMATCH_MAPPING = 'data/mxm_mappings.csv'
+CSV_HEADER = ['msd_id', 'msd_artist', 'msd_title', 'mxm_id', 'mxm_artist', 'mxm_title']
 
-
-def main():
+def mapping_to_csv():
 
     # do not execute script again if lyrics have already been scraped
     if os.path.exists(CSV_MUSIXMATCH_MAPPING):
-        print('{0} already exists. Exiting...'.format(CSV_MUSIXMATCH_MAPPING))
+        print('{0} already exists. Skipping csv conversion.'.format(CSV_MUSIXMATCH_MAPPING))
         return
 
     count = 0
-    with open(CSV_MUSIXMATCH_MAPPING, 'w', encoding='utf-8') as csvfile:
+    with open(CSV_MUSIXMATCH_MAPPING, mode='w', encoding='utf-8', newline='') as csvfile:
         csvwriter = csv.writer(csvfile)
-        header = ['msd_id', 'msd_artist', 'msd_title', 'mxm_id', 'mxm_artist', 'mxm_title']
         csvwriter.writerow(header)
 
         with open(FILE_MUSIXMATCH_MAPPING, mode='r', encoding="utf-8") as f:
@@ -44,9 +44,48 @@ def main():
                 except Exception as exc:
                     print(exc)
 
-    print('Processed {0} songs'.format(count))
+    print('Processed {0} songs.'.format(count))
 
     return
+
+
+def get_api_token():
+
+    api_token = None
+
+    with open('data/api.txt', 'r') as f:
+        api_token = f.read()
+
+    return api_token
+
+
+def scrape_lyrics():
+
+    api_token = get_api_token()
+    api = genius.Genius(client_access_token=api_token)
+
+    songs_matched = 0
+    with open(CSV_MUSIXMATCH_MAPPING, mode='r', encoding='utf-8', newline='') as csvfile:
+        csvreader = csv.DictReader(csvfile, fieldnames=CSV_HEADER)
+        first = True
+        for row in csvreader:
+            # skip header row
+            if first:
+                first = False
+                continue
+
+            song = api.search_song(row['msd_title'], row['msd_artist'])
+            if song:
+                songs_matched += 1
+                print(song)
+
+    return
+
+
+def main():
+
+    mapping_to_csv()
+    scrape_lyrics()
 
 
 if __name__ == '__main__':
